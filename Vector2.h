0396ebe1c555ecf64ddfb529cf358f055dc94558
Demohstens <demohstens@gmail.com>
2023-08-30 15:42:34 +0200

    Deleted redundant code and added Vector2.h

---
+++ Vector2.h
@@ -0,0 +1,27 @@
+#include "Point.h"
+
+class Vector2 {
+private:
+    class Coordinate {
+        private:
+            Vector2* super;
+            float value;
+        public:
+            //Constructor
+            Coordinate(Vector2* v, float num) : super(v), value(num) {};
+            //Overloads
+            float operator =(float num);
+    };
+public:
+    Coordinate x;
+    Coordinate y;
+    //Constructors
+    //Default constructor
+    Vector2(float& x_init, float& y_init) : x(this, x_init), y(this, y_init)  {};
+    Vector2() = default;
+    //Constructor from Point
+    Vector2(Point& point);
+    //Constructor from two Points
+    Vector2(Point& point, Point& point2); 
+};
+
